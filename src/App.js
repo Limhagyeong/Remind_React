2814bb3c23215467da5cecad7b83a84d2903ddde
Limhagyeong <aamumu88@gmail.com>
2024-10-27 19:31:53 +0900

    update

--- src/App.js
+++ src/App.js
@@ -87,6 +87,52 @@ function Create(props) {
     </article>
   );
 }
+function Update(props) {
+  // props는 외부에서 내부로 전달받은 값 => 내부에서 조작이 불가함
+  // state 형태로 바꿔서 내부에서 조작이 가능하도록 해야함
+  // 초기값은 외부에서 받은 값으로 설정해두고 이후 state를 통해 사용자가 입력한 값으로 업데이트 시킴 (onChange + state 활용)
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            value={title}
+            onChange={(event) => {
+              // 리액트에서 onChange는 값 변경 자체가 트리거
+              setTitle(event.target.title.value);
+            }}
+          />
+        </p>
+        <p>
+          <textarea
+            name="body"
+            placeholder="body"
+            value={body}
+            onChange={(event) => {
+              setBody(event.target.body);
+            }}
+          ></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Update"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 function App() {
   // const _mode = useState('WELCOME');
   // console.log(_mode);
@@ -103,6 +149,7 @@ function App() {
   ]);
 
   let content = null;
+  let contextControl = null;
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
   } else if (mode === 'READ') {
@@ -115,6 +162,19 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+    contextControl = (
+      <li>
+        <a
+          href={'/update/' + selectedId}
+          onClick={(event) => {
+            event.preventDefault();
+            setMode('UPDATE');
+          }}
+        >
+          Update
+        </a>
+      </li>
+    );
   } else if (mode === 'CREATE') {
     content = (
       <Create
@@ -132,6 +192,37 @@ function App() {
         }}
       ></Create>
     ); // Create 컴포넌트를 띄움
+  } else if (mode === 'UPDATE') {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === selectedId) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = (
+      <Update
+        title={title}
+        body={body}
+        onUpdate={(updateTitle, updateBody) => {
+          const newTopics = [...topics]; // 기존 토픽 배열 깊은 복사
+          const updatedTopic = {
+            id: selectedId,
+            title: updateTitle,
+            body: updateBody,
+          }; // 사용자가 입력한 정보
+          for (let i = 0; i < newTopics.length; i++) {
+            if (newTopics[i].id === selectedId) {
+              newTopics[i] = updatedTopic; // 업데이트
+              break; // 더 돌릴 필요없음
+            }
+          }
+          setTopics(newTopics);
+          setMode('READ');
+        }}
+      ></Update>
+    );
   }
   return (
     <div>
@@ -146,21 +237,24 @@ function App() {
         onShow={(selectedId) => {
           setMode('READ');
           setSelectedId(selectedId);
-          console.log(topics);
         }}
       ></Nav>
       {content}
-      <a
-        href="/create"
-        onClick={(event) => {
-          event.preventDefault();
-          setMode('CREATE');
-        }}
-      >
-        Create
-      </a>
+      <ul>
+        <li>
+          <a
+            href="/create"
+            onClick={(event) => {
+              event.preventDefault();
+              setMode('CREATE');
+            }}
+          >
+            Create
+          </a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
-
 export default App;
