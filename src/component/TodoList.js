2aa5320b7970872710e4b6de16531819b9f95a3b
Limhagyeong <aamumu88@gmail.com>
2024-10-28 10:56:03 +0900

    update(1)

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 
-// Create, Update, Delete 연습
 function TodoList() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const [updateMode, setUpdateMode] = useState('N');
 
   const addTask = () => {
     if (task.trim() !== '') {
       const newTask = { id: tasks.length, task: task };
       setTasks([...tasks, newTask]);
       setTask('');
-      console.log([...tasks, newTask]);
     }
   };
 
@@ -20,16 +20,63 @@ function TodoList() {
         {tasks.map((eachTask) => (
           <>
             <li className="eachList" key={eachTask.id}>
-              <input type="checkbox" class="item-checkbox" />
+              <input type="checkbox" className="item-checkbox" />
               {eachTask.task}
-              <button>update</button>
+              {updateMode === 'N' && (
+                <button
+                  onClick={() => {
+                    setSelectedId(eachTask.id);
+                    setUpdateMode('Y');
+                  }}
+                >
+                  update
+                </button>
+              )}
+
               <button>Delete</button>
             </li>
+            {selectedId === eachTask.id && updateMode === 'Y' && (
+              <Update id={selectedId} task={eachTask.task} />
+            )}
           </>
         ))}
       </ul>
     );
   }
+
+  function Update(props) {
+    const [updateTask, setUpdateTask] = useState(props.task);
+    return (
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const ChangeTask = [
+            { id: props.selectedId, task: event.target.value },
+          ];
+          console.log('ch:'.ChangeTask);
+          for (let i = 0; i < tasks.length; i++) {
+            if (tasks[i].id === ChangeTask.id) {
+              tasks[i] = ChangeTask;
+            }
+          }
+          // console.log(tasks);
+          setUpdateMode('N');
+        }}
+      >
+        <input
+          type="text"
+          // value={props.task} => 바로 받으면 조작할 수 없음
+          value={updateTask}
+          onChange={(event) => {
+            setUpdateTask(event.target.value);
+          }}
+        ></input>
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
+
+  const saveUpdate = () => {};
 
   return (
     <div className="TodoList">
